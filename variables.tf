84acd1b9648710681a9bce3a2ad3261b722caa17
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-07-30 15:55:57 +0200

    Adding variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,31 @@
-variable "management_group_id" {
-    description = "Management group id where the custom policies should be created. Required if policies is defined."
-    default = null
+variable "name" {
+  description = "Short name of this policy assignment."
 }
 
-variable "policies" {
-    description = "A list of custom policies to apply to management group defined by `management_group_id`."
-    type = list(object({ name = string, description = string, mode = string, policy_rule = string, metadata = string, parameters = string }))
-    default = []
+variable "description" {
+  description = "The Azure Region in which to create resource."
+}
+
+variable "location" {
+  description = "The Azure Region in which to create resource."
+}
+
+variable "scope" {
+    description = "The Scope at which the Policy Assignment should be applied."
+}
+
+variable "custom_policy" {
+    description = "A custom policy to create and apply to management group defined by `management_group_id`."
+    type = object({ name = string, description = string, mode = string, policy_rule = string, metadata = string, parameters = string })
+    default = {}
+}
+
+variable "policy_definition_id" {
+    description = "The ID of the Policy Definition to be applied at the scope. If `custom_policy` variable is defined it will ignore this."
+    default = null
 }
 
 variable "assignments" {
     description = "A list of policies to assign to scope."
-    type = list(object({ name = string, description = string, definition_id = string, parameters = string, not_scopes = list(string) }))
+    type = list(object({ parameters = string, scope = string, not_scopes = list(string) }))
 }
